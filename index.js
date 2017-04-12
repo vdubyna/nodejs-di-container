cc22123fc30af737124c08713cc51bb5f3016d1c
Volodymyr Dubyna <vdubyna@speroteck.com>
2017-04-12 21:34:50 +0300

    Add standard style to follow

--- index.js
+++ index.js
@@ -1,179 +1,170 @@
-let _ = require('lodash');
-let dwell = require('dwell');
+let _ = require('lodash')
+let dwell = require('dwell')
 
 class Ioc {
+  /**
+   *
+   * @param config
+   */
+  Constructor (config = {}) {
+    this.appPath = config.appPath
 
-    /**
-     *
-     * @param config
-     */
-    constructor(config = {}) {
-        this.appPath = config.appPath;
+    this.classes = new Map()
+    this.objects = new Map()
+    this.aliases = new Map()
 
-        this.classes = new Map();
-        this.objects = new Map();
-        this.aliases = new Map();
+    _.map(config.aliases, (className, aliasName) => {
+      this.aliases.set(aliasName, className)
+    })
+    _.map(config.classes, (classInstance, classKey) => {
+      this.classes.set(classKey, classInstance)
+    })
+    _.map(config.objects, (objectInstance, objectKey) => {
+      this.objects.set(objectKey, objectInstance)
+    })
+  }
 
-        _.map(config.aliases, (className, aliasName) => {
-            this.aliases.set(aliasName, className);
-        });
-        _.map(config.classes, (classInstance, classKey) => {
-            this.classes.set(classKey, classInstance);
-        });
-        _.map(config.objects, (objectInstance, objectKey) => {
-            this.objects.set(objectKey, objectInstance);
-        });
+  /**
+   * Only for registered classes works like factory
+   * You can define own factory and dependencies
+   * All dependencies should go to object trough constructor
+   *
+   * @param libName
+   * @param dependencies
+   * @param injector
+   * @returns {*}
+   */
+  make (libName, dependencies = [], injector = false) {
+    if (!this.classes.has(libName)) {
+      throw new Error(`Class ${libName} is not registered`)
     }
 
-    /**
-     * Only for registered classes works like factory
-     * You can define own factory and dependencies
-     * All dependencies should go to object trough constructor
-     *
-     * @param libName
-     * @param dependencies
-     * @param injector
-     * @returns {*}
-     */
-    make(libName, dependencies = [], injector = false) {
-        if (!this.classes.has(libName)) {
-            throw new Error(`Class ${libName} is not registered`);
-        }
+    let newLib = this.classes.get(libName)
 
-        let newLib = this.classes.get(libName);
-
-        if (!_.isEmpty(dependencies) && injector === false) {
-            return this.injector(newLib, dependencies);
-        }
-
-        if (!_.isEmpty(dependencies) && _.isFunction(injector)) {
-            return injector(dependencies);
-        }
-
-        return this._makeNewLib(newLib);
+    if (!_.isEmpty(dependencies) && injector === false) {
+      return this.injector(newLib, dependencies)
     }
 
-    /**
-     * Return lib if not exist, create and register to reuse it
-     *
-     * @param libName
-     * @returns {Object}
-     */
-    use(libName) {
-
-        if (this.objects.has(libName)) {
-            return this.objects.get(libName)
-        }
-
-        let newLib = this._autoload(libName);
-
-        let dependencies = _.map(dwell.inspect(newLib), (injection) => {
-            return this.use(injection);
-        });
-
-        return new newLib(...dependencies);
+    if (!_.isEmpty(dependencies) && _.isFunction(injector)) {
+      return injector(dependencies)
     }
 
-    /**
-     *
-     * @param libName
-     * @param fn
-     * @returns {Ioc}
-     */
-    register(libName, fn) {
-        // If function or class - set to classes
-        if (_.isFunction(fn)) {
-            this.classes.set(libName, fn);
-            return this;
-        }
+    return this._makeNewLib(newLib)
+  }
 
-        // If object/string/array - set to objects
-        this.objects.set(libName, fn);
-        return this;
+  /**
+   * Return lib if not exist, create and register to reuse it
+   *
+   * @param libName
+   * @returns {Object}
+   */
+  use (libName) {
+    if (this.objects.has(libName)) {
+      return this.objects.get(libName)
     }
 
-    /**
-     *
-     * @param newLib
-     * @param dependencies
-     * @returns {*}
-     */
-    static injector (newLib, dependencies) {
-        return new newLib(...dependencies);
+    let NewLib = this._autoload(libName)
+
+    let dependencies = _.map(dwell.inspect(NewLib), (injection) => {
+      return this.use(injection)
+    })
+
+    return new NewLib(...dependencies)
+  }
+
+  /**
+   *
+   * @param libName
+   * @param fn
+   * @returns {Ioc}
+   */
+  register (libName, fn) {
+    // If function or class - set to classes
+    if (_.isFunction(fn)) {
+      this.classes.set(libName, fn)
+      return this
     }
 
-    /**
-     * Always returns new lib without registration in the container
-     * All dependencies are registered in the container and reused
-     *
-     * @param newLib
-     * @returns {*}
-     * @private
-     */
-    _makeNewLib(newLib) {
-        let dependencies = _.map(dwell.inspect(newLib), (injection) => {
-            return this.use(injection);
-        });
+    // If object/string/array - set to objects
+    this.objects.set(libName, fn)
+    return this
+  }
 
-        return this.injector(newLib, dependencies);
-    };
+  /**
+   *
+   * @param NewLib
+   * @param dependencies
+   * @returns {*}
+   */
+  static injector (NewLib, dependencies) {
+    return new NewLib(...dependencies)
+  }
 
-    /**
-     * All classes are registered in the container to reuse
-     *
-     * @param libName
-     * @returns {Function}
-     * @private
-     */
-    _autoload(libName) {
-        libName = this._filterLibName(libName);
+  /**
+   * Always returns new lib without registration in the container
+   * All dependencies are registered in the container and reused
+   *
+   * @param newLib
+   * @returns {*}
+   * @private
+   */
+  _makeNewLib (newLib) {
+    let dependencies = _.map(dwell.inspect(newLib), (injection) => {
+      return this.use(injection)
+    })
 
-        try {
-            // Try load registered library or core library
-            if (this.classes.has(libName)) {
-                return this.classes.get(libName)
-            }
-            this.classes.set(libName, require(libName));
-        } catch (e) {
-            // Try load app library
-            this.classes.set(libName, this._autoloadAppLibrary(libName));
-        }
+    return this.injector(newLib, dependencies)
+  };
 
-        return this.classes.get(libName);
+  /**
+   * All classes are registered in the container to reuse
+   *
+   * @param libName
+   * @returns {Function}
+   * @private
+   */
+  _autoload (libName) {
+    libName = this._filterLibName(libName)
+
+    try {
+      // Try load registered library or core library
+      if (this.classes.has(libName)) {
+        return this.classes.get(libName)
+      }
+      this.classes.set(libName, require(libName))
+    } catch (e) {
+      // Try load app library
+      this.classes.set(libName, this._autoloadAppLibrary(libName))
     }
 
-    /**
-     *
-     * @param libName
-     * @returns {any|*}
-     * @private
-     */
-    _autoloadAppLibrary(libName) {
-        libName = this._filterLibName(libName);
+    return this.classes.get(libName)
+  }
 
-        try {
-            return require(this.appPath + libName.replace('_', '/'));
-        } catch (e) {
-            throw new Error(`${libName} could not be loaded`);
-        }
-    };
+  /**
+   *
+   * @param libName
+   * @returns {any|*}
+   * @private
+   */
+  _autoloadAppLibrary (libName) {
+    libName = this._filterLibName(libName)
 
-    /**
-     *
-     * @param libName
-     * @returns {string}
-     * @private
-     */
-    _filterLibName(libName) {
-        return (this.aliases.has(libName)) ? this.aliases.get(libName) : libName;
+    try {
+      return require(this.appPath + libName.replace('_', '/'))
+    } catch (e) {
+      throw new Error(`${libName} could not be loaded`)
     }
+  };
+
+  /**
+   *
+   * @param libName
+   * @returns {string}
+   * @private
+   */
+  _filterLibName (libName) {
+    return (this.aliases.has(libName)) ? this.aliases.get(libName) : libName
+  }
 }
 
-
-module.exports = Ioc;
-
-
-
-
-
-
+module.exports = Ioc
